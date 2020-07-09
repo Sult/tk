602c8391af4d0bddfe5ad8f49119acc4afb50c96
Ruud Renssen <ruud@vicompany.nl>
2020-07-09 12:45:12 +0200

    draft queries 0.1

---
+++ tk_data/queries.py
@@ -0,0 +1,24 @@
+# All votes
+votes = Stemming.objects
+
+# Filter all votes about laws
+votes = Stemming.objects.filter(besluit__zaak__soort = 'Wetgeving')
+
+# Exclude votes that have not reached the floor
+votes = Stemming.objects.exclude(besluit__stemming_soort='')
+
+# Exclude votes about merging city councils
+votes = Stemming.objects.exclude(Q(besluit__zaak__titel__icontains='gemeente') and Q(besluit__zaak__titel__icontains='amenvoeg'))
+
+# Exclude votes about cities reorganisations
+votes = Stemming.objects.exclude(Q(besluit__zaak__titel__icontains='gemeente') and Q(besluit__zaak__titel__icontains='herindeling'))
+
+# Exclude votes about EU publications
+votes = Stemming.objects.exclude(besluit__zaak__titel__icontains='PbEU')
+
+# Combined query:
+votes = Stemming.objects.filter(besluit__zaak__soort = 'Wetgeving') \
+    .exclude(besluit__stemming_soort='') \
+    .exclude(Q(besluit__zaak__titel__icontains='gemeente') and Q(besluit__zaak__titel__icontains='amenvoeg')) \
+    .exclude(Q(besluit__zaak__titel__icontains='gemeente') and Q(besluit__zaak__titel__icontains='herindeling')) \
+    .exclude(besluit__zaak__titel__icontains='PbEU')
